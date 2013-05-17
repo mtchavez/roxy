cdabcef8461e5464c2199250cbef66d4caf2677a
Chavez <nailsbrokeandfell@gmail.com>
2013-05-17 10:53:12 -0700

    Refactoring

--- roxy/server.go
+++ roxy/server.go
@@ -9,62 +9,6 @@ import (
 	"net"
 )
 
-var commandToNum = map[string]byte{
-	"RpbErrorResp":         0,
-	"RpbPingReq":           1,
-	"RpbPingResp":          2,
-	"RpbGetClientIdReq":    3,
-	"RpbGetClientIdResp":   4,
-	"RpbSetClientIdReq":    5,
-	"RpbSetClientIdResp":   6,
-	"RpbGetServerInfoReq":  7,
-	"RpbGetServerInfoResp": 8,
-	"RpbGetReq":            9,
-	"RpbGetResp":           10,
-	"RpbPutReq":            11,
-	"RpbPutResp":           12,
-	"RpbDelReq":            13,
-	"RpbDelResp":           14,
-	"RpbListBucketsReq":    15,
-	"RpbListBucketsResp":   16,
-	"RpbListKeysReq":       17,
-	"RpbListKeysResp":      18,
-	"RpbGetBucketReq":      19,
-	"RpbGetBucketResp":     20,
-	"RpbSetBucketReq":      21,
-	"RpbSetBucketResp":     22,
-	"RpbMapRedReq":         23,
-	"RpbMapRedResp":        24,
-}
-
-var numToCommand = map[int]string{
-	0:  "RpbErrorResp",
-	1:  "RpbPingReq",
-	2:  "RpbPingResp",
-	3:  "RpbGetClientIdReq",
-	4:  "RpbGetClientIdResp",
-	5:  "RpbSetClientIdReq",
-	6:  "RpbSetClientIdResp",
-	7:  "RpbGetServerInfoReq",
-	8:  "RpbGetServerInfoResp",
-	9:  "RpbGetReq",
-	10: "RpbGetResp",
-	11: "RpbPutReq",
-	12: "RpbPutResp",
-	13: "RpbDelReq",
-	14: "RpbDelResp",
-	15: "RpbListBucketsReq",
-	16: "RpbListBucketsResp",
-	17: "RpbListKeysReq",
-	18: "RpbListKeysResp",
-	19: "RpbGetBucketReq",
-	20: "RpbGetBucketResp",
-	21: "RpbSetBucketReq",
-	22: "RpbSetBucketResp",
-	23: "RpbMapRedReq",
-	24: "RpbMapRedResp",
-}
-
 type Request struct {
 	Conn  net.Conn
 	Quit  chan bool
@@ -75,7 +19,7 @@ type Request struct {
 func (req *Request) Close() {
 	fmt.Println("Closing connection")
 	req.Conn.Close()
-	// req.Quit <- true
+	req.Quit <- true
 	fmt.Println("Finished closing connection")
 }
 
@@ -93,61 +37,68 @@ func (req *Request) Read() (buffer []byte, err error) {
 	buffer = make([]byte, 512)
 	_, err = req.Conn.Read(buffer)
 	if err != nil {
-		req.Close()
 		fmt.Println("Error reading")
+		return
 	}
-	fmt.Println("RequestStruct: ", numToCommand[int(buffer[4])])
 	buffer = BufferedResponse(buffer)
+	fmt.Println("RequestStruct: ", numToCommand[int(buffer[4])])
+	fmt.Println("Read: ", buffer)
+	fmt.Println()
 	return
 }
 
 func (req *Request) Write(buffer []byte) {
-	fmt.Printf("\n[Writing]\n")
-	fmt.Println(buffer)
 	req.Conn.Write(buffer)
-	fmt.Println("[Finished Writing]\n")
+	fmt.Println("Wrote: ", buffer)
+	fmt.Println()
 }
 
 func RequestHandler(conn net.Conn) {
 	quit := make(chan bool)
 	in := make(chan []byte, 2)
 	req := &Request{Conn: conn, Quit: quit}
-	go RequestReader(req, in)
-	go RequestSender(req, in)
-}
-
-func RequestSender(req *Request, in chan []byte) {
+	go RequestReader(req, in, quit)
 	for {
-		fmt.Println("[Begin] RequestSender")
-		output := <-in
-		conn, err := net.Dial("tcp", ":8087")
-		if err != nil {
-			fmt.Println("Error connection to riak")
+		select {
+		case incoming := <-in:
+			HandleRequest(req, incoming)
+		case <-quit:
+			break
 		}
-		_, err = conn.Write(output)
-		if err != nil {
-			fmt.Println("Error writing to riak")
-			return
-		}
-		respraw := make([]byte, 512)
-		_, err = conn.Read(respraw)
-		fmt.Println("ResponseStruct: ", numToCommand[int(respraw[4])])
-		respraw = BufferedResponse(respraw)
-		req.Write(respraw)
-		conn.Close()
-		fmt.Println("[End] RequestSender")
 	}
 }
 
-func RequestReader(req *Request, in chan []byte) {
+func HandleRequest(req *Request, incomming []byte) {
+	conn, err := net.Dial("tcp", ":8087")
+	if err != nil {
+		fmt.Println("Error connection to riak")
+	}
+	_, err = conn.Write(incomming)
+	if err != nil {
+		fmt.Println("Error writing to riak")
+		return
+	}
+	respraw := make([]byte, 512)
+	_, err = conn.Read(respraw)
+	fmt.Println("ResponseStruct: ", numToCommand[int(respraw[4])])
+	respraw = BufferedResponse(respraw)
+	req.Write(respraw)
+	conn.Close()
+	fmt.Println()
+}
+
+func RequestReader(req *Request, in chan []byte, quit chan bool) {
 	for {
-		buf, err := req.Read()
-		if err == nil {
-			fmt.Printf("Buffer: \n%v\n", buf)
+		select {
+		case <-quit:
+			break
+		default:
+			buf, err := req.Read()
+			if err != nil {
+				req.Close()
+				return
+			}
 			in <- buf
-		} else {
-			req.Close()
-			return
 		}
 	}
 }
