9383dcf7c1d322673bf9119b16f691a0f5bff86c
Chavez <nailsbrokeandfell@gmail.com>
2013-06-04 13:24:15 -0700

    Comment config.go

--- roxy/config.go
+++ roxy/config.go
@@ -6,10 +6,14 @@ import (
 
 var Configuration *Config
 
+// Config holds the Doc for the parsed toml config file
 type Config struct {
 	Doc toml.Document
 }
 
+// Uses github.com/laurent22/toml-go/toml package to parse
+// a toml config file. Parses once and returns configuration
+// if already parsed and set.
 func ParseConfig(filepath string) *Config {
 	if Configuration != nil {
 		return Configuration
