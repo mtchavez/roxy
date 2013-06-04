5f6da63b552566c1108c1e273878c8d0e1173cd2
Chavez <nailsbrokeandfell@gmail.com>
2013-06-04 11:13:25 -0700

    Add a proxy listener for tests

--- roxy/setup_test.go
+++ roxy/setup_test.go
@@ -2,6 +2,7 @@ package roxy
 
 import (
 	. "launchpad.net/gocheck"
+	"net"
 	"testing"
 )
 
@@ -11,3 +12,36 @@ func Test(t *testing.T) { TestingT(t) }
 type MySuite struct{}
 
 var _ = Suite(&MySuite{})
+
+func (s *MySuite) RunInProxy(fn func(net.Listener, chan net.Conn, chan bool)) {
+	ch := make(chan net.Conn, 1)
+	close := make(chan bool, 1)
+	listener := s.ProxyServer(ch, close)
+	fn(listener, ch, close)
+	listener.Close()
+	close <- true
+}
+
+func (s *MySuite) ProxyServer(ch chan net.Conn, close chan bool) net.Listener {
+	serverString := "127.0.0.1:8089"
+	listenerConn, netErr := net.Listen("tcp", serverString)
+	if netErr != nil {
+		return listenerConn
+	}
+	go func() {
+		for {
+			select {
+			case <-close:
+				return
+			default:
+				conn, err := listenerConn.Accept()
+				if err != nil {
+					break
+				} else {
+					ch <- conn
+				}
+			}
+		}
+	}()
+	return listenerConn
+}
